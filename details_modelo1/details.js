1c8605c360474d6e5f0fa7be9d00c28c416979ef
allskiin <89169894+allskiin@users.noreply.github.com>
2022-04-04 00:24:20 +0100

    Add files via upload

--- details_modelo1/details.js
+++ details_modelo1/details.js
@@ -38,3 +38,69 @@ function changeColor() {
 
 sizes.forEach(size => size.addEventListener('click', changeSize))
 colors.forEach(c => c.addEventListener('click', changeColor))
+
+
+
+function showModal() {
+    var element = document.getElementById("modal");
+    element.classList.add("show-modal");
+}
+
+function closeModal() {
+    var element = document.getElementById("modal");
+    element.classList.remove("show-modal");
+}
+
+
+$(function() {
+    $('#cardnumber').payment('formatCardNumber');
+    $('#cardexpiration').payment('formatCardExpiry');
+    $('#cardcvc').payment('formatCardCVC');
+
+    $('#cardnumber').keyup(function(event) {
+        $('#label-cardnumber').empty().append($(this).val());
+    });
+
+    $('#cardexpiration').keyup(function(event) {
+        var data = $(this).val() + '<span>' + $('#cardcvc').val() + '</span>';
+        $('#label-cardexpiration').empty().append(data);
+    });
+
+    $('#cardcvc').keyup(function(event) {
+        var data = $('#cardexpiration').val() + '<span>' + $(this).val() + '</span>';
+        $('#label-cardexpiration').empty().append(data);
+    });
+
+    $('.button-cta').on('click', function() {
+        var proceed = true;
+        $(".field input").each(function() {
+            $(this).parent().find('path').each(function() {
+                $(this).attr('fill', '#dddfe6');
+            });
+
+            if (!$.trim($(this).val())) {
+                $(this).parent().find('path').each(function() {
+                    $(this).attr('fill', '#f1404b');
+                    proceed = false;
+                });
+
+                if (!proceed) {
+                    $(this).parent().find('svg').animate({ opacity: '0.1' }, "slow");
+                    $(this).parent().find('svg').animate({ opacity: '1' }, "slow");
+                    $(this).parent().find('svg').animate({ opacity: '0.1' }, "slow");
+                    $(this).parent().find('svg').animate({ opacity: '1' }, "slow");
+                }
+            }
+        });
+
+        if (proceed) //everything looks good! proceed purchase...
+        {
+            $('.field').find('path').each(function() {
+                $(this).attr('fill', '#3ac569');
+            });
+            $('.payment').fadeToggle('slow', function() {
+                $('.paid').fadeToggle('slow', 'linear');
+            });
+        }
+    });
+});
